ec1b2048ac408bd0ac3563bef65d54777fedd9f9
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-05-06 00:51:50 +0530

    Project

---
+++ Background-image-scroll-Effect/js/script.js
@@ -0,0 +1,11 @@
+let bgimageel = document.getElementById("bg-image");
+
+window.addEventListener("scroll" , ()=> {
+    upadateImage()
+})
+
+function upadateImage() {
+    bgimageel.style.opacity = 1 - window.pageYOffset / 800
+
+    bgimageel.style.backgroundSize = 160 - window.pageYOffset / 12 + "%"
+}
