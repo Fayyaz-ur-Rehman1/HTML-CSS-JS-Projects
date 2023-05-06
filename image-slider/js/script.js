4f29832130761710d50262780482584f5f168f38
Fayyaz-ur-Rehman1 <fayyazurrehman289@gmail.com>
2023-05-06 11:48:47 +0530

    Image-Slider

---
+++ image-slider/js/script.js
@@ -0,0 +1,36 @@
+let nextel = document.querySelector(".next");
+let imagecontainerel = document.querySelector(".image-container");
+let imgel = document.querySelector("img");
+let prevel = document.querySelector(".prev");
+
+let currentimg = 1
+
+let timeout;
+
+nextel.addEventListener("click", () => {
+    currentimg++
+    clearTimeout(timeout)
+    updateimg()
+})
+
+prevel.addEventListener("click", () => {
+
+    currentimg--
+    clearTimeout(timeout)
+    updateimg()
+})
+
+updateimg()
+
+function updateimg() {
+    if (currentimg > imgel.length) {
+        currentimg = 1;
+    } else if (currentimg < 1) {
+        currentimg = imgel.length
+    }
+    imagecontainerel.style.transform = `translateX(-${(currentimg - 1) * 500}px)`;
+    timeout = setTimeout(() => {
+        currentimg++
+        updateimg()
+    }, 3000)
+}  
